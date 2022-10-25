f2993afde64b0660eff06c8ad5c15417ad816211
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2022-10-25 18:46:24 +0300

    upgrade

---
+++ src/components/Reducers/index.js
@@ -0,0 +1,16 @@
+
+const initState=
+{
+    posts:
+    [
+        { id: '1', title: 'Squirtle Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectutur'},
+        { id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectutur'},
+        { id: '3', title: 'a Hrlix Fossil was Found', body: 'Lorem ipsum, dolor sit amet consectutur'}
+    ]
+}
+const Reducers = (state=initState,action) => {
+  return state;
+  
+}
+
+export default Reducers
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+
 import { Link } from 'react-router-dom'
 import './style.css';
+import {connect} from 'react-redux';
 
 class Home extends Component {
-  state = {
-    posts:[]
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(
-      res => {
-        console.log(res);
-        this.setState({
-          posts:res.data.slice(0,10)
-        });
-      }
-    )
-  }
-
+  
   render () {
-    const { posts } = this.state
+    const { posts } = this.props
     const postList = posts.length ? (
       posts.map(post => {
         return (
@@ -47,5 +34,11 @@ class Home extends Component {
     )
   }
 }
+const mapStateToProps = (state)=>
+{
+  return{
+    posts:state.posts
+  }
+}
 
-export default Home
+export default connect(mapStateToProps)(Home) 
